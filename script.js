3508d8c337bb17edb2f4950d0ba2e7df93689666
davdstya <darayastore01@gmail.com>
2023-07-21 11:12:27 +0700

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+function tampilkanAlert() {
+    var nilai = prompt("Masukkan nilai:");
+    var grade;
+
+    if (nilai >= 86 && nilai <= 100) {
+        grade = "A";
+    } else if (nilai >= 76 && nilai <= 85) {
+        grade = "B";
+    } else if (nilai >= 56 && nilai <= 75) {
+        grade = "C";
+    } else if (nilai >= 45 && nilai <= 55) {
+        grade = "D";
+    } else if (nilai >= 0 && nilai <= 44) {
+        grade = "E";
+    } else {
+        alert("Nilai diluar range");
+        return;
+    }
+
+    alert("Nilai Anda: " + nilai + " Hasilnya adalah " + grade);
+}
